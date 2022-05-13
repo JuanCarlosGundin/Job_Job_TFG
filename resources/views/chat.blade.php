0d8bf7642b2d381094f89b30debd537dc49f6098
Pol <polgarciamoreno392@gmail.com>
2022-05-13 17:04:31 +0200

    Estoy trabajando en el chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -107,78 +107,42 @@
                 <div class="mensaje-inicio">
                     <p class="mensaje-text">Bienvenido al chat</p>
                 </div>
+            </div>                
+            <div class="mensaje-1">
+                <div class="chat-mensaje-1">
+                    <p class="mensaje-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p class="mensaje-hora">20:45</p>          
+                </div>
             </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
+            <div class="mensaje-1">
+                <div class="chat-mensaje-1">
+                    <p class="mensaje-text">Lorem ipsum dolor sit amet</p>
+                    <p class="mensaje-hora">20:45</p>          
+                </div>
+            </div> 
+            <div class="mensaje-1">
+                <div class="chat-mensaje-1">
+                    <p class="mensaje-text">Lorem ipsum</p>
+                    <p class="mensaje-hora">20:45</p>          
+                </div>
             </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
+            <div class="mensaje-1">
+                <div class="chat-mensaje-1">
+                    <p class="mensaje-text">Lorem ipsum</p>
+                    <p class="mensaje-hora">20:45</p>          
+                </div>
             </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
+            <div class="mensaje-1">
+                <div class="chat-mensaje-1">
+                    <p class="mensaje-text">Lorem ipsum</p>
+                    <p class="mensaje-hora">20:45</p>          
+                </div>
             </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
-            </div>
-            <div class="chat-mensaje-1">
-                <p class="mensaje-text">nvfnvfnj</p>
-                <p class="mensaje-hora">20:45</p>          
-            </div>
-            <div class="chat-mensaje-2">
-                <p class="mensaje-text">nvfnvfn jnvfnvfn nvfnvfnjnv fnvfnjnv fnvfnj</p>
-                <p class="mensaje-hora">20:45</p>                    
+            <div class="mensaje-2">
+                <div class="chat-mensaje-2">
+                    <p class="mensaje-text">Lorem ipsum</p>
+                    <p class="mensaje-hora">20:45</p>          
+                </div>
             </div>
         </div>
         <div class="chat-main-send">
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -84,8 +84,8 @@
         </div>
     </div>
     
-    {{-- <div id="contenidoajax">
-    </div> --}}
+    <div id="contenidoajax">
+    </div>
     {{-- Eliminar cuenta modalbox --}}
     <div id="modal-eliminar" class="modal-eliminar-user">
         <div class="modal-content" id="modal_content">
