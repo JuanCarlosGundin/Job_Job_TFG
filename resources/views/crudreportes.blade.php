20f94cb077da64f265efa0ae20a92c768911dd8d
David <100006394.joan23@fje.edu>
2022-05-21 19:21:39 +0200

    filtros comentados

--- resources/views/crudreportes.blade.php
+++ resources/views/crudreportes.blade.php
@@ -8,6 +8,20 @@
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
 </head>
 <body>
+  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <li>
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="abierta" id="abierta" onclick="leerreportesJS()" checked>
+        <label class="form-check-label" for="abierta">Abierta</label>
+        </div>
+    </li>
+    <li>
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="cerrada" id="cerrada" onclick="leerreportesJS()" checked>
+        <label class="form-check-label" for="cerrada">Cerrada</label>
+        </div>
+    </li>
+</ul>
   <h2>Buscar reporte</h2>
     <input type="text" onkeyup="leerreportesJS()" id="filtro">
     <hr>
