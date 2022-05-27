cc1724c3282be53935ef31e51356079b300ed6d6
David <100006394.joan23@fje.edu>
2022-05-27 18:18:22 +0200

    pull con diego para pillar estilos de pol

--- resources/views/crudreportes.blade.php
+++ resources/views/crudreportes.blade.php
@@ -12,10 +12,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GESTIONAR REPORTES</title>
     <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/15d3106c42.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../public/css/admin.css">
+    <link rel="stylesheet" href="../public/css/style-pol.css">
+    <title>Reportes</title>
 </head>
-<body>
+<body class="admin">
   <h2>Buscar reporte</h2>
     <input type="text" onkeyup="leerreportesJS()" id="filtro">
     <hr>
